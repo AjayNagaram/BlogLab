f5865c588e639f8c53d5f231fcd165fccb1fa215
AjayNagaram <33978713+AjayNagaram@users.noreply.github.com>
2023-01-19 19:52:45 +0530

    Model project completed. Repository Project created with Account repository.

---
+++ BlogLab.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserCreate : ApplicationUserLogin
+    {
+        [MinLength(10, ErrorMessage = "Must be atlease 10 to 30 charecters")]
+        [MaxLength(30, ErrorMessage = "Must be atlease 10 to 30 charecters")]
+        public string Fullname { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [MaxLength(30, ErrorMessage = "Must be atlease 10 to 50 charecters")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+    }
+}
---
+++ BlogLab.Repository/IAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogLab.Models.Account;
+using Microsoft.AspNetCore.Identity;
+
+namespace BlogLab.Repository
+{
+    internal interface IAccountRepository
+    {
+        public Task<IdentityResult> CreateAsync(ApplicationUserIdentity user,
+            CancellationToken token);
+
+        public Task<ApplicationUserIdentity> GetByUsernameAsync(string normalizedUsername, 
+            CancellationToken token);
+    }
+}
